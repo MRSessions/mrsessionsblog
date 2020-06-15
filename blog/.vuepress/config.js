2ff161e834e71ec2888098e1f38ed29e148a2a44
Matt Sessions <mattsessions@live.com>
2020-06-14 19:28:14 -0500

    Initial Commit

---
+++ blog/.vuepress/config.js
@@ -0,0 +1,83 @@
+module.exports = {
+  title: 'mrsessions blog',
+  description: 'This is a blog site built by VuePress',
+  theme: '@vuepress/theme-blog', // OR shortcut: @vuepress/blog
+  themeConfig: {
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#modifyblogpluginoptions
+     */
+    modifyBlogPluginOptions(blogPluginOptions) {
+      return blogPluginOptions
+    },
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#nav
+     */
+    nav: [
+      {
+        text: 'Blog',
+        link: '/',
+      },
+      {
+        text: 'About',
+        link: '/about/',
+      },
+    ],
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#footer
+     */
+    footer: {
+      contact: [
+// codepen
+// codesandbox
+// facebook
+// github
+// gitlab
+// instagram
+// linkedin
+// mail
+// messenger
+// music
+// phone
+// twitter
+// video
+// web
+// youtube
+        {
+          type: 'linkedin',
+          link: 'https://www.linkedin.com/in/mrsessionsit/',
+        },
+        {
+          // type: 'twitter',
+          // link: 'https://twitter.com/_ulivz',
+        },
+      ],
+      copyright: [
+        {
+          text: 'Privacy Policy',
+          link: 'https://www.privacypolicies.com/generic/'
+          // link: 'https://policies.google.com/privacy?hl=en-US',
+        },
+        {
+          text: 'MIT Licensed | Copyright © 2020-present mrsessions.com',
+          link: '',
+        },
+      ],
+    },
+    comment: {
+      service: 'disqus',
+      shortname: 'mrsessions blog',
+    },
+  },
+  // plugins: {
+  //   '@vssue/vuepress-plugin-vssue': {
+  //     // set `platform` rather than `api`
+  //     platform: 'github',
+
+  //     // all other options of Vssue are allowed
+  //     owner: 'mrsessions',
+  //     repo: 'mrsessionsblog',
+  //     clientId: '6bbd0914809cb31ffab3',
+  //     clientSecret: '756a0d167583e7d6dbd03486287d800ec5bc6e32',
+  //   },
+  // },
+}
---
+++ blog/about.md
@@ -0,0 +1,10 @@
+![me](https://media-exp1.licdn.com/dms/image/C5603AQH5qLD1K6nZ3w/profile-displayphoto-shrink_100_100/0?e=1597881600&v=beta&t=TMG-GgN-ngdGMu0GpKqBk9DZnxwXztE-EACCmoVfVeU)
+<h1>Hi, I'm Matt</h1>
+<br/>
+<p>I am a software developer out of the Greater Nashville Area, TN. Born and raised</p>
+<p>I have been a software developer since the age of 24 (born in 1990), but have always been into computers and technology. Since then, I have always had more focus towards C# and SQL development as well as other related technologies to the two.
+</p>
+<p>I have decided to start a blog in this 'wonderful' year of 2020, because... well..., some of us need more things to do these days. Plus, I want to be able to help other developers along the way, so I'm hoping the information I share, can help others out.</p>
+<br/>
+<p>Thank you,<br/>Matt Sessions</p>
+
